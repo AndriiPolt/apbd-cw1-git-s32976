e3d788e4212ab6dff3ec96334840dd3a3323b162
andy270505 <116752626+andy270505@users.noreply.github.com>
2026-03-18 20:52:20 +0100

    Add StatisticsHelper class with Sum method

---
+++ APBD/StatisticsHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace MyConsoleApp;
+
+public class StatisticsHelper
+{
+    public int Sum(int[] values)
+    {
+        int sum = 0;
+        foreach (var v in values)
+        {
+            sum += v;
+        }
+        return sum;
+    }
+}
